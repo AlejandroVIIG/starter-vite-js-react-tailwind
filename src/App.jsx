573e0ce29a21e2fd508728751f94b4bb18034551
Alejandro Villavicencio <alejandro.viig@gmail.com>
2023-11-28 20:25:48 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  console.log()
+
+  return (
+    <>
+      <h1 className="text-center p-2 font-bold">
+        Starter React - Vite - Javascript - TailwindCSS
+      </h1>
+    </>
+  )
+}
+
+export default App
